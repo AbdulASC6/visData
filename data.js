6223460ce01b10661f26ef5856e12047a471cc9e
AbdulASC6 <52749242+AbdulASC6@users.noreply.github.com>
2019-08-01 14:16:12 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,623 @@
+const data =
+{
+    "results": [
+    {
+    "gender": "Male",
+    "name": {
+    "title": "mr",
+    "first": "Alexandre Singh"
+    },
+    "location": {
+    "street": "1305 richmond ave",
+    "city": "Chelsea",
+    "state": "Northwest Territories",
+    "postcode": "E4O 0F3",
+    "coordinates": {
+    "latitude": "-64.5176",
+    "longitude": "-6.1035"
+    },
+    "timezone": {
+    "offset": "+5:00",
+    "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+    }
+    },
+    "email": "alexandre.singh@example.com",
+    "login": {
+    "uuid": "0da2c26a-8a0f-44bd-a41b-a559ad2bb7f5",
+    "username": "bigpanda677",
+    "password": "creampie",
+    "salt": "FMd0qTYQ",
+    "md5": "6dd5d162b79825c94516786d0ff58bf9",
+    "sha1": "a2ab062f0493757864d3ae29e025164f9c1c951f",
+    "sha256": "2edb87ab9642b40fafe3308b686c85d511fea67facbb21b0017ebbbef0b81a08"
+    },
+    "dob": {
+    "date": "1961-11-30T00:28:03Z",
+    "age": 57
+    },
+    "registered": {
+    "date": "2008-03-05T02:16:28Z",
+    "age": 11
+    },
+    "phone": "681-774-9320",
+    "cell": "492-746-4537",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/57.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/57.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/57.jpg"
+    },
+    "nat": "CA"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "madame",
+    "first": "Mary Chevalier"
+    },
+    "location": {
+    "street": "9785 rue de bonnel",
+    "city": "Rohr (so)",
+    "state": "Zug",
+    "postcode": 9430,
+    "coordinates": {
+    "latitude": "-44.0531",
+    "longitude": "-155.8314"
+    },
+    "timezone": {
+    "offset": "-6:00",
+    "description": "Central Time (US & Canada), Mexico City"
+    }
+    },
+    "email": "mary.chevalier@example.com",
+    "login": {
+    "uuid": "6e4b6a68-6c26-4803-93aa-273f7bffc827",
+    "username": "heavyzebra617",
+    "password": "over",
+    "salt": "XGlBCV4F",
+    "md5": "3e43fe5aaf6da6b7ff500c26cc39342d",
+    "sha1": "2f7d55b4b014c2b61158ae35f44da0b53c6afda9",
+    "sha256": "b21f8a667f6010618dc90ef73216b2cbd6c09f5e0de338b933a9f5933d165c59"
+    },
+    "dob": {
+    "date": "1984-04-09T22:24:20Z",
+    "age": 35
+    },
+    "registered": {
+    "date": "2013-10-31T14:37:06Z",
+    "age": 5
+    },
+    "phone": "(611)-044-1449",
+    "cell": "(721)-963-1144",
+    "id": {
+    "name": "AVS",
+    "value": "756.2665.7913.58"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/75.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/75.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/75.jpg"
+    },
+    "nat": "CH"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "ms",
+    "first": "Martina Völker"
+    },
+    "location": {
+    "street": "mittelweg 157",
+    "city": "Frankfurt (oder)",
+    "state": "Sachsen-anhalt",
+    "postcode": 40207,
+    "coordinates": {
+    "latitude": "-8.3931",
+    "longitude": "97.0649"
+    },
+    "timezone": {
+    "offset": "-4:00",
+    "description": "Atlantic Time (Canada), Caracas, La Paz"
+    }
+    },
+    "email": "martina.völker@example.com",
+    "login": {
+    "uuid": "e8a6f9cb-cdea-4321-b727-7fb31ea51267",
+    "username": "browncat571",
+    "password": "sanford",
+    "salt": "VbGuQmuT",
+    "md5": "7f099352f4a608a31baa4cf49c69cc95",
+    "sha1": "a093d1225b52474d19563f27664e42b92a8cf112",
+    "sha256": "c5738f9ecd4b10bee046599f35ab4f70cdac764e8f336a70cf106fcb669e0c74"
+    },
+    "dob": {
+    "date": "1985-09-15T20:23:13Z",
+    "age": 33
+    },
+    "registered": {
+    "date": "2009-09-11T18:40:50Z",
+    "age": 9
+    },
+    "phone": "0682-4023968",
+    "cell": "0175-6167100",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/13.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/13.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/13.jpg"
+    },
+    "nat": "DE"
+    },
+    {
+    "gender": "Male",
+    "name": {
+    "title": "mr",
+    "first": "محمدمهدی نجاتی"
+    },
+    "location": {
+    "street": "1991 شهید همت",
+    "city": "نیشابور",
+    "state": "آذربایجان شرقی",
+    "postcode": 41003,
+    "coordinates": {
+    "latitude": "-52.0682",
+    "longitude": "53.2393"
+    },
+    "timezone": {
+    "offset": "+2:00",
+    "description": "Kaliningrad, South Africa"
+    }
+    },
+    "email": "محمدمهدی.نجاتی@example.com",
+    "login": {
+    "uuid": "a8be259d-4fbe-4c40-a4d5-c085a35bc318",
+    "username": "redbutterfly248",
+    "password": "forrest",
+    "salt": "x6PceK6e",
+    "md5": "bf9bdfce26f6d2c46a56c2e66a208466",
+    "sha1": "df957287aae9f3ce93343435ce1cc6506107b3df",
+    "sha256": "1f61dae197739bb09f164ec3aa2bd05236d57f0b89ca29dc62aed7cf5438d1d4"
+    },
+    "dob": {
+    "date": "1991-04-08T01:32:08Z",
+    "age": 28
+    },
+    "registered": {
+    "date": "2004-08-28T20:52:00Z",
+    "age": 14
+    },
+    "phone": "030-68776149",
+    "cell": "0946-431-5249",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/2.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/2.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/2.jpg"
+    },
+    "nat": "IR"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "miss",
+    "first": "مارال گلشن"
+    },
+    "location": {
+    "street": "7832 امیرکبیر",
+    "city": "آبادان",
+    "state": "خراسان شمالی",
+    "postcode": 30568,
+    "coordinates": {
+    "latitude": "49.9043",
+    "longitude": "-14.2346"
+    },
+    "timezone": {
+    "offset": "+7:00",
+    "description": "Bangkok, Hanoi, Jakarta"
+    }
+    },
+    "email": "مارال.گلشن@example.com",
+    "login": {
+    "uuid": "2ac36045-fad8-4929-8200-ca1cb838c055",
+    "username": "blueelephant212",
+    "password": "hairy",
+    "salt": "B1vP3cex",
+    "md5": "31c22c527175e59e895809bc195981a3",
+    "sha1": "d8b07d8fc8acfb106c4dae3e2ea1f2cc544cd82d",
+    "sha256": "c427e4254bed2275a433b8b564b1e82b7d9af69ad1fa1a20d97f86ee24d6067e"
+    },
+    "dob": {
+    "date": "1971-01-21T18:29:56Z",
+    "age": 48
+    },
+    "registered": {
+    "date": "2015-07-07T02:56:34Z",
+    "age": 4
+    },
+    "phone": "041-86865636",
+    "cell": "0962-106-7831",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/87.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/87.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/87.jpg"
+    },
+    "nat": "IR"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "mrs",
+    "first": "Teresa Marin"
+    },
+    "location": {
+    "street": "8688 calle de alcalá",
+    "city": "Málaga",
+    "state": "La Rioja",
+    "postcode": 34723,
+    "coordinates": {
+    "latitude": "77.5932",
+    "longitude": "61.8111"
+    },
+    "timezone": {
+    "offset": "-2:00",
+    "description": "Mid-Atlantic"
+    }
+    },
+    "email": "teresa.marin@example.com",
+    "login": {
+    "uuid": "019e15a1-2e20-451a-9bb9-27f79661837e",
+    "username": "ticklishtiger501",
+    "password": "stryker",
+    "salt": "zHRp0r9j",
+    "md5": "9874e3fc929cfe59524daa44a4d69ada",
+    "sha1": "76e5103d6cf35ac635b5505701eada90ee4d17e4",
+    "sha256": "323bb7762adf78a8f415c348520083e444c5feec12e62561325818d8230a1f53"
+    },
+    "dob": {
+    "date": "1969-02-04T20:17:08Z",
+    "age": 50
+    },
+    "registered": {
+    "date": "2003-10-19T20:48:39Z",
+    "age": 15
+    },
+    "phone": "999-611-219",
+    "cell": "639-175-948",
+    "id": {
+    "name": "DNI",
+    "value": "37692254-P"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/75.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/75.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/75.jpg"
+    },
+    "nat": "ES"
+    },
+    {
+    "gender": "Male",
+    "name": {
+    "title": "mr",
+    "first": "Timoteüs Laman"
+    },
+    "location": {
+    "street": "9387 bokstraat",
+    "city": "Lelystad",
+    "state": "Limburg",
+    "postcode": 79953,
+    "coordinates": {
+    "latitude": "28.4508",
+    "longitude": "5.1427"
+    },
+    "timezone": {
+    "offset": "+3:30",
+    "description": "Tehran"
+    }
+    },
+    "email": "timoteüs.laman@example.com",
+    "login": {
+    "uuid": "9e5dc205-37d5-4ad0-9562-317de73dada9",
+    "username": "tinysnake879",
+    "password": "13579",
+    "salt": "VUtKHoC5",
+    "md5": "ae26109b022da7035606dc5b89bdef1b",
+    "sha1": "c97a8af356005b8c4406474bd13c48acd3864d51",
+    "sha256": "928de004657935fc1d510811f750935dc8586949713815cc4c2d777c12344c2f"
+    },
+    "dob": {
+    "date": "1975-07-31T18:47:46Z",
+    "age": 44
+    },
+    "registered": {
+    "date": "2006-10-25T00:59:55Z",
+    "age": 12
+    },
+    "phone": "(028)-012-5791",
+    "cell": "(104)-679-3002",
+    "id": {
+    "name": "BSN",
+    "value": "26306296"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/78.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/78.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/78.jpg"
+    },
+    "nat": "NL"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "mrs",
+    "first": "Elea Mikkelborg"
+    },
+    "location": {
+    "street": "marta steinsviks vei 4234",
+    "city": "Toft",
+    "state": "Vestfold",
+    "postcode": "5106",
+    "coordinates": {
+    "latitude": "33.7027",
+    "longitude": "-48.9409"
+    },
+    "timezone": {
+    "offset": "-2:00",
+    "description": "Mid-Atlantic"
+    }
+    },
+    "email": "elea.mikkelborg@example.com",
+    "login": {
+    "uuid": "aa421acd-526f-4e86-ad48-c468b4f4f2dd",
+    "username": "organicgorilla574",
+    "password": "cole",
+    "salt": "yXt2nPut",
+    "md5": "3ebae8fd88f1e0f53d102b5ec69f0dbb",
+    "sha1": "5282fbf500c40f572341221adc7758788faf1a95",
+    "sha256": "8c97af18fd19e71ad2d0e236dbc9cf1a82ff5aab9d66201e4b7603a686696075"
+    },
+    "dob": {
+    "date": "1978-07-28T01:31:33Z",
+    "age": 41
+    },
+    "registered": {
+    "date": "2007-08-18T00:26:13Z",
+    "age": 11
+    },
+    "phone": "88532842",
+    "cell": "45473502",
+    "id": {
+    "name": "FN",
+    "value": "28077849302"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/83.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/83.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/83.jpg"
+    },
+    "nat": "NO"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "ms",
+    "first": "Virginia Garrido"
+    },
+    "location": {
+    "street": "459 avenida de burgos",
+    "city": "Cuenca",
+    "state": "Galicia",
+    "postcode": 35279,
+    "coordinates": {
+    "latitude": "70.2308",
+    "longitude": "-19.3393"
+    },
+    "timezone": {
+    "offset": "-6:00",
+    "description": "Central Time (US & Canada), Mexico City"
+    }
+    },
+    "email": "virginia.garrido@example.com",
+    "login": {
+    "uuid": "71cff911-32f4-4f2c-9f7e-5bae43879b3e",
+    "username": "beautifulostrich192",
+    "password": "right",
+    "salt": "W1k7ooes",
+    "md5": "6d1ef46c6ded810af04eafa3fb64b9c0",
+    "sha1": "13fb915a7ea014019c2030ccf9200dff66c0631f",
+    "sha256": "fe721a8e5c2296666c899806716c938b8b487665ee8fc84ba84cea34fba10626"
+    },
+    "dob": {
+    "date": "1993-03-08T14:22:17Z",
+    "age": 26
+    },
+    "registered": {
+    "date": "2003-12-24T19:50:17Z",
+    "age": 15
+    },
+    "phone": "989-896-469",
+    "cell": "680-863-760",
+    "id": {
+    "name": "DNI",
+    "value": "72487375-R"
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/51.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/51.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/51.jpg"
+    },
+    "nat": "ES"
+    },
+    {
+    "gender": "Male",
+    "name": {
+    "title": "mr",
+    "first": "Laureano Almeida"
+    },
+    "location": {
+    "street": "6207 rua são pedro ",
+    "city": "Itapetininga",
+    "state": "Roraima",
+    "postcode": 54586,
+    "coordinates": {
+    "latitude": "-14.8679",
+    "longitude": "27.9221"
+    },
+    "timezone": {
+    "offset": "-4:00",
+    "description": "Atlantic Time (Canada), Caracas, La Paz"
+    }
+    },
+    "email": "laureano.almeida@example.com",
+    "login": {
+    "uuid": "f177ccb4-9837-4e0e-9d94-c03e0148cf21",
+    "username": "brownostrich684",
+    "password": "wallet",
+    "salt": "68jI5WX2",
+    "md5": "6196b433e1822d2f704dd71f9361d704",
+    "sha1": "82c909df0b5bbdc7c4b6f7a4b78d6524904014c9",
+    "sha256": "2e78ebcbc126292c97c9bce62a670724e9b997502d62c4df73c76fc5042b71f6"
+    },
+    "dob": {
+    "date": "1966-08-30T00:33:11Z",
+    "age": 52
+    },
+    "registered": {
+    "date": "2013-11-20T19:11:10Z",
+    "age": 5
+    },
+    "phone": "(08) 4758-2832",
+    "cell": "(46) 8354-1018",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/47.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/47.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/47.jpg"
+    },
+    "nat": "BR"
+    },
+    {
+    "gender": "Male",
+    "name": {
+    "title": "mr",
+    "first": "Klaus peter Riemann"
+    },
+    "location": {
+    "street": "mühlenstraße 153",
+    "city": "germering",
+    "state": "bayern",
+    "postcode": 48879,
+    "coordinates": {
+    "latitude": "-75.0543",
+    "longitude": "-16.4676"
+    },
+    "timezone": {
+    "offset": "+5:00",
+    "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+    }
+    },
+    "email": "klaus peter.riemann@example.com",
+    "login": {
+    "uuid": "acf41044-a50a-43fb-8e38-6638dfe6c5d3",
+    "username": "whiteswan911",
+    "password": "mirage",
+    "salt": "bTmXS3zZ",
+    "md5": "e072fb0948e0e5bedae3db3d3f5ee9b6",
+    "sha1": "772b12eff57ae6e5401e79a0a31c3a2f003ef6a4",
+    "sha256": "999b89fc0b721a035b1da9c3b7f8c65f9008df1a4be43c8d7175769cb4716502"
+    },
+    "dob": {
+    "date": "1949-09-08T04:40:53Z",
+    "age": 69
+    },
+    "registered": {
+    "date": "2010-03-28T16:31:02Z",
+    "age": 9
+    },
+    "phone": "0712-8576422",
+    "cell": "0171-4303674",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/men/77.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/77.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/77.jpg"
+    },
+    "nat": "DE"
+    },
+    {
+    "gender": "Female",
+    "name": {
+    "title": "ms",
+    "first": "Wally Schmitz"
+    },
+    "location": {
+    "street": "lindenweg 178",
+    "city": "Mainz",
+    "state": "Brandenburg",
+    "postcode": 59587,
+    "coordinates": {
+    "latitude": "31.7917",
+    "longitude": "-60.9214"
+    },
+    "timezone": {
+    "offset": "0:00",
+    "description": "Western Europe Time, London, Lisbon, Casablanca"
+    }
+    },
+    "email": "wally.schmitz@example.com",
+    "login": {
+    "uuid": "53968a5d-ee85-44c6-b687-b690370686bf",
+    "username": "purplepeacock389",
+    "password": "clippers",
+    "salt": "SSBQkU2g",
+    "md5": "49d7785115344e46304d16f7ced941bd",
+    "sha1": "483db72c96a7957a678fd3d317134a9acca23f68",
+    "sha256": "23c5e2cb7da7638c530a55814cfae6d6bf212779c48041a6ce8a33d7013f5c9d"
+    },
+    "dob": {
+    "date": "1972-01-22T09:31:17Z",
+    "age": 47
+    },
+    "registered": {
+    "date": "2009-07-15T22:55:26Z",
+    "age": 10
+    },
+    "phone": "0262-6424885",
+    "cell": "0179-5796957",
+    "id": {
+    "name": "",
+    "value": null
+    },
+    "picture": {
+    "large": "https://randomuser.me/api/portraits/women/94.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/women/94.jpg",
+    "thumbnail": "https://randomuser.me/api/portraits/thumb/women/94.jpg"
+    },
+    "nat": "DE"
+    }
+    ],
+    "info": {
+    "seed": "4d8fa89010b67609",
+    "results": 12,
+    "page": 1,
+    "version": "1.2"
+    }
+}
---
+++ main.js
@@ -0,0 +1,50 @@
+// person 1
+let rand = Math.floor(Math.random() * 12 + 1);
+for(let i = 0; i < rand; i++){
+    document.getElementById('name_1').innerHTML = data.results[i].name.first;
+    document.getElementById('gen_1').innerHTML = data.results[i].gender;
+    document.getElementById('age_1').innerHTML = data.results[i].dob.age;
+    document.getElementById('job_1').innerText = job();
+    document.getElementById('hob_1').innerText = hobby();
+    document.getElementById('loc_1').innerHTML = data.results[i].location.state; 
+    document.getElementById('ema_1').innerHTML = data.results[i].email;
+    document.getElementById('pho_1').innerHTML = data.results[i].phone;
+    const profile1 = document.getElementById('pic_1');
+    profile1.setAttribute('src',data.results[i].picture.large)
+}
+
+
+
+// person 2
+let rand_2 = Math.floor(Math.random() * 12 + 1);
+for(let i = 0; i < rand_2; i++){
+    document.getElementById('name_2').innerHTML = data.results[i].name.first;
+    document.getElementById('gen_2').innerHTML = data.results[i].gender;
+    document.getElementById('age_2').innerHTML = data.results[i].dob.age;
+    document.getElementById('job_2').innerText = job();
+    document.getElementById('hob_2').innerText = hobby();
+    document.getElementById('loc_2').innerHTML = data.results[i].location.state;
+    document.getElementById('ema_2').innerHTML = data.results[i].email;
+    document.getElementById('pho_2').innerHTML = data.results[i].phone;
+    const profile2 = document.getElementById('pic_2');
+    profile2.setAttribute('src',data.results[i].picture.large);
+}
+
+//jobs
+
+function rand_stuff(x){
+    let random = Math.floor(Math.random() * x.length);
+    return random;
+}
+function job(){
+    let occupation = ["Designer","Footballer","Firefighter","Art critic","Dental hygienist","Factory worker","Career criminal","Pilot","Landowner","Barber","Leaflet distributor","Sports coach","Tax Fraudist"];
+    let idx = rand_stuff(occupation);
+    jobs = occupation[idx];
+    return jobs;
+}
+function hobby(){
+    let fun = ["Hunting","Antiquing","Shoe Collecting",'Cooking','Baking','Painting','Drawing','Video Games','Reading Manga', 'Watching Anime'];
+    let idx = rand_stuff(fun);
+    hobbies = fun[idx];
+    return hobbies;
+}
